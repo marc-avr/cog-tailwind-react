df06d4eb351c11f73939c98a046f48102618df38
Marcus Everson <meverson@appliedvr.io>
2023-04-11 15:23:45 -0700

    color fix

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -87,12 +87,12 @@ const ForgotPassword = () => {
                         <h5 className="text-center text-2xl font-medium mb-2">Reset Password</h5>
                         <h5 className="text-center text-md font-normal mb-3">Step 2 of 2</h5>
                         {errorMessage && 
-                            <div className="bg-green-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
                                 {errorMessage}
                             </div>
                         }
                          {successMessage && 
-                            <div className="bg-red-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
+                            <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
                                 {successMessage}
                             </div>
                         }
